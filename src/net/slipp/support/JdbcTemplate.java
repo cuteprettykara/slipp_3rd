149c5d0b7e3b0d40edee14cf81cca6aebf140d51
cuteprettykara <cuteprettykara@gmail.com>
2019-04-01 14:47:56 +0900

    가변인자 적용

--- src/net/slipp/support/JdbcTemplate.java
+++ src/net/slipp/support/JdbcTemplate.java
@@ -25,7 +25,28 @@ public class JdbcTemplate {
 		}
 	}
 	
-	public <T> T executeQuery(String sql, PreparedStatementSetter pss, RowMapper<T> rm) throws SQLException {
+	public void executeUpdate(String sql, Object... parameters) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = ConnectionManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			
+			for (int i = 0; i < parameters.length; i++) {
+				pstmt.setObject(i+1, parameters[i]);
+			}
+			
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			
+		} finally {
+			if (pstmt != null) pstmt.close();
+			if (conn != null) conn.close();
+		}
+	}
+	
+	public <T> T executeQuery(String sql, RowMapper<T> rm,  PreparedStatementSetter pss) throws SQLException {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -50,7 +71,41 @@ public class JdbcTemplate {
 			
 		} finally {
 			if (rs != null) rs.close();
+			if (pstmt != null) conn.close();
 			if (conn != null) conn.close();
+		}
+		
+		return t;
+	}
+	
+	public <T> T executeQuery(String sql, RowMapper<T> rm, Object... parameters) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		T t = null;
+		
+		try {
+			conn = ConnectionManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			
+			for (int i = 0; i < parameters.length; i++) {
+				pstmt.setObject(i+1, parameters[i]);
+			}
+			
+			rs = pstmt.executeQuery();
+			
+			if (!rs.next()) {
+				return null;
+			}
+			
+			t = rm.mapRow(rs);
+			
+		} catch (Exception e) {
+			
+		} finally {
+			if (rs != null) rs.close();
+			if (pstmt != null) conn.close();
 			if (conn != null) conn.close();
 		}
 		
--- src/net/slipp/user/UserDAO.java
+++ src/net/slipp/user/UserDAO.java
@@ -12,67 +12,29 @@ public class UserDAO {
 
 	public void addUser(User user) throws SQLException {
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
-		
-		PreparedStatementSetter pss = new PreparedStatementSetter() {
-			@Override
-			public void setParameters(PreparedStatement pstmt) throws SQLException {
-				pstmt.setString(1, user.getUserId());
-				pstmt.setString(2, user.getPassword());
-				pstmt.setString(3, user.getName());
-				pstmt.setString(4, user.getEmail());
-			}
-		};
-		
 		String sql = "insert into USERS values(?,?,?,?)";
-		jdbcTemplate.executeUpdate(sql, pss);
+		jdbcTemplate.executeUpdate(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
 	}
 
 	public void removeUser(String userId) throws SQLException {
-		
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
-		
-		PreparedStatementSetter pss = new PreparedStatementSetter() {
-			@Override
-			public void setParameters(PreparedStatement pstmt) throws SQLException {
-				pstmt.setString(1, userId);
-			}
-		};
-		 
 		String sql ="delete from USERS where userId = ?";
-		jdbcTemplate.executeUpdate(sql, pss);
-
+		jdbcTemplate.executeUpdate(sql, userId);
 	}
 
 	public void update(User user) throws SQLException {
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
-		
-		PreparedStatementSetter pss = new PreparedStatementSetter() {
-			@Override
-			public void setParameters(PreparedStatement pstmt) throws SQLException {
-				pstmt.setString(1, user.getPassword());
-				pstmt.setString(2, user.getName());
-				pstmt.setString(3, user.getEmail());
-				pstmt.setString(4, user.getUserId());
-			}
-		};
 		
 		String sql = "update USERS" +
 				 	 "   set password = ?" + 
 				 	 "     , name     = ?" + 
 				 	 "     , email    = ?" +
 				 	 " where userId   = ?";
-		jdbcTemplate.executeUpdate(sql, pss);
+		jdbcTemplate.executeUpdate(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
 	}
 	
 	public User findById(String userId) throws SQLException {
 		JdbcTemplate jdbcTemplate = new JdbcTemplate();
-		
-		PreparedStatementSetter pss = new PreparedStatementSetter() {
-			@Override
-			public void setParameters(PreparedStatement pstmt) throws SQLException {
-				pstmt.setString(1, userId);
-			}
-		};
 		
 		RowMapper<User> rm = new RowMapper<User>() {
 			@Override
@@ -88,6 +50,6 @@ public class UserDAO {
 		};
 		
 		String sql = "select * from USERS where userId = ?"; 
-		return jdbcTemplate.executeQuery(sql, pss, rm);
+		return jdbcTemplate.executeQuery(sql, rm, userId);
 	}
 }
